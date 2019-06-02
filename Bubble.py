f76b578433aef5388b5861f7927327293e5ab24c
hjh_pro <hjh_pro@1997.com>
2019-06-02 15:12:46 +0800

    Bubble

---
+++ Bubble.py
@@ -0,0 +1,19 @@
+#coding:utf8
+#对比相邻的两个元素，大的往后移
+def Bubble(elist):
+    n=len(elist)
+    found = True
+    for i in range(n):
+        for j in range(n-i-1):
+            if elist[j] > elist[j+1]:
+                elist[j],elist[j+1] = elist[j+1],elist[j]
+                found = False
+        if found:
+            return
+if __name__ == "__main__":
+    a=[2,5,1,7,9,4,3,0,13,-4,-8,-1]
+    # a=[1,2,4,5,7,8,9]
+    Bubble(a)
+    print(a)
+
+
